54b5f77e2815aad3878678ba936a3f00c6e47ddd
soupierbucket <42323328+soupierbucket@users.noreply.github.com>
2020-07-10 18:44:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,23 +15,26 @@ model = pickle.load(open('model.pkl','rb'))
 # app
 app = Flask(__name__)
 
+print('Outside predict')
+
 # routes
 @app.route('/', methods=['POST'])
 
 def predict():
+    print('predict_start')
     # get data
     data = request.get_json(force=True)
-
+    print('predict_start_data')
     # convert data into dataframe
     data.update((x, [y]) for x, y in data.items())
     data_df = pd.DataFrame.from_dict(data)
-
+    print('predict_start_dataframe')
     # predictions
     result = model.predict(data_df)
-
+    print('Before_output')
     # send back to browser
     output = {'results': int(result[0])}
-
+    print(result)
     # return data
     return jsonify(results=output)
 
